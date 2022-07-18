12977cf6ab5f26708714e043b3a642ed13e8f531
Vincentkeio <97959564+Vincentkeio@users.noreply.github.com>
2022-07-18 16:05:08 +0800

    Update system.sh

--- setup/system.sh
+++ setup/system.sh
@@ -339,9 +339,9 @@ fi
 # which is where bind9 will be running. Obviously don't do this before
 # installing bind9 or else apt won't be able to resolve a server to
 # download bind9 from.
-rm -f /etc/resolv.conf
+#rm -f /etc/resolv.conf
 management/editconf.py /etc/systemd/resolved.conf DNSStubListener=no
-echo "nameserver 1.1.1.1 127.0.0.1" > /etc/resolv.conf
+#echo "nameserver 1.1.1.1 127.0.0.1" > /etc/resolv.conf
 
 # Restart the DNS services.
 
